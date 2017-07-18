bcb75176ceab701ecc67a042263324ca5a3135f4
Eric Hiller <ehiller@hiller.pro>
2017-07-18 10:42:20 -0700

    Gave a description of the new python install procedure for proper embedded->full conversion

--- config/pkg/python/install.ps1
+++ config/pkg/python/install.ps1
@@ -41,11 +41,31 @@ Install-PackageFromURL($Package)
 Remove-Item -ErrorAction Ignore -WarningAction SilentlyContinue -Path ( Join-Path ( Join-Path (Join-Path $Env:Basedir $OMEGA_CONF.sysdir) $Package.name ) "python36._pth" )
 Remove-Item -ErrorAction Ignore -WarningAction SilentlyContinue -Path ( Join-Path ( Join-Path (Join-Path $Env:Basedir $OMEGA_CONF.sysdir) $Package.name ) "python36._pth~" )
 
+
 # YOU __MAY__ HAVE TO EXTRACT THE python36.zip into /Lib/
 # YOU MAY have to copy an older version of lib2to3 and libimport from an installed version into your site-packages
 # You DO HAVE TO UNPACK!!!
 7z x ( Join-Path ( Join-Path (Join-Path $Env:Basedir $OMEGA_CONF.sysdir) $Package.name ) "python36.zip" ) "-o$( Join-Path ( Join-Path (Join-Path $Env:Basedir $OMEGA_CONF.sysdir) $Package.name ) "Lib" )"
 Remove-Item -ErrorAction Ignore -WarningAction SilentlyContinue -Path ( Join-Path ( Join-Path (Join-Path $Env:Basedir $OMEGA_CONF.sysdir) $Package.name ) "python36.zip" )
+
+
+///// NO ///// DO NOT KEEP THE ZIP ///// DO THIS::::::
+
+
+Converting the Embedded Python into Full
+Embedded Python is using compiled main libraries (apparent by the *.pyc file endings) to convert, remove all of these and use *.py source libraries
+
+
+Get-ChildItem -Recurse "C:\Users\ehiller\AppData\Local\omega\system\python\Lib\" | Where-Object { $_.LastWriteTime -lt (Get-Date -year 2017 -month 7 -day 16)}
+
+(Get-ChildItem -Recurse "C:\Users\ehiller\AppData\Local\omega\system\python\Lib\" -include "*.pyc" | Where-Object { $_.Directory.Name.CompareTo("__pycache__") -and $_.Directory.Name.CompareTo("Lib") } ).Directory | Get-Unique | Remove-Item -Recurse -Force
+
+Get-ChildItem -Recurse "C:\Users\ehiller\AppData\Local\omega\system\python\Lib\" -include "*.pyc" | Where-Object { -not $_.Directory.Name.CompareTo("Lib") } | Remove-Item -Recurse -Force
+
+Simply grab the `.tgz` from the python ftp and copy over the `/Lib` into the windows _Python_ install directory `/Lib`
+
+
+//////
 
 ########################################
 ################ STEP 3 ################
